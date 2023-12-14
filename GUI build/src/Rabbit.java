5b1b78d38577510e0e48a592dbb4af904dc23c2f
s-agustafson <s-agustafson@486E294160.lwsd.k12wa.org>
2023-12-14 09:59:44 -0800

    s

--- GUI build/src/Rabbit.java
+++ GUI build/src/Rabbit.java
@@ -1,7 +1,24 @@
 
 public class Rabbit extends Animal{
+	public int reproductionClock;
 	public Rabbit() {
 		super();
+		reproductionClock = 0;
 	}
-	//public void 
+	
+	public void move(
+		if(reproductionClock > 5) {
+			
+		}
+		int tempx = (int)(Math.random() * 7);
+		int tempy = (int)(Math.random() * 7);
+		currx += (tempx-3);
+		curry += (tempy-3);
+		if(currx > gameloop.MAX_X) currx -= gameloop.MAX_X;
+		if(currx < 0) currx += gameloop.MAX_X;
+		if(curry > gameloop.MAX_Y) currx -= gameloop.MAX_Y;
+		if(curry < 0) currx += gameloop.MAX_Y;
+		reproductionClock++;
+			
+	}
 }
